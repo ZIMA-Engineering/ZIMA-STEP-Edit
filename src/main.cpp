10c9cdc5fd83945198ae0b7d7734ec026a3ea9e2
Jakub Skokan <jakub.skokan@havefun.cz>
2012-03-05 18:46:15 +0100

    Fixed translation encoding.

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@ int main(int argc, char *argv[])
 
 	QTextCodec::setCodecForTr(QTextCodec::codecForName("utf8"));
 	QTextCodec::setCodecForCStrings(QTextCodec::codecForName("utf8"));
+	QTextCodec::setCodecForLocale(QTextCodec::codecForName("utf8"));
 
 	QApplication a(argc, argv);
 
